ca99514250d167b4533a2db3d841404aad255aea
JorgeCarlos99 <o.sanjorge1@gmail.com>
2022-12-13 23:01:41 +0100

    sprites King

--- Assets/Scripts/CheckBoxPopUp.cs
+++ Assets/Scripts/CheckBoxPopUp.cs
@@ -1,11 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class CheckBoxPopUp : MonoBehaviour
 {
     public GameObject message;
     bool collided = false;
+    public float delay = 0.1f;
+    private string fullText;
+    private string fullTextMin1;
+    private bool done = false;
+    private string currentText = "";
+    private string doneText;
+    public TextMeshProUGUI texto;
+
+    void Start()
+    {
+        fullText = "hola que tal dios que guapo esto.";
+        fullTextMin1 = "hola que tal dios que guapo esto";
+    }
+
+    void Update()
+    {
+        if (texto.text.Equals(fullTextMin1))
+        {
+            done = true;
+        }
+        else
+        {
+            done = false;
+        }
+    }
 
     IEnumerator OnTriggerEnter2D(Collider2D collider)
     {
@@ -16,6 +42,7 @@ public class CheckBoxPopUp : MonoBehaviour
             if (collided == true) //check if collided is still true
             {
                 message.SetActive(true);
+                StartCoroutine(ShowText());
             }
         }
     }
@@ -28,6 +55,24 @@ public class CheckBoxPopUp : MonoBehaviour
             yield return new WaitForSeconds(5);
             message.SetActive(false);
             collided = false;
+        }
+    }
+
+    IEnumerator ShowText()
+    {
+        Debug.Log(" currentText " + currentText);
+        if (done)
+        {
+            Debug.Log(" currentText1 " + currentText);
+
+            currentText = "";
+        }
+        Debug.Log(" currentText1 " + currentText);
+        for (int i = 0; i < fullText.Length; i++)
+        {
+            currentText = fullText.Substring(0, i);
+            texto.text = currentText;
+            yield return new WaitForSeconds(delay);
         }
     }
 }
--- Assets/Scripts/PopUpText1.cs
+++ Assets/Scripts/PopUpText1.cs
@@ -1,27 +1,54 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using TMPro;
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+// using TMPro;
 
-public class PopUpText1 : MonoBehaviour
-{
-    public float delay = 0.1f;
-    private string fullText;
-    private string currentText = "";
-    public TextMeshProUGUI texto;
+// public class PopUpText1 : MonoBehaviour
+// {
+//     public float delay = 0.1f;
+//     private string fullText;
+//     private string fullTextMin1;
+//     private bool done = false;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        fullText = "hola que tal dios que guapo esto.";
-        StartCoroutine(ShowText());
-    }
+//     private string currentText = "";
+//     private string doneText;
+//     public TextMeshProUGUI texto;
 
-    IEnumerator ShowText() {
-        for (int i = 0; i < fullText.Length; i++) {
-            currentText = fullText.Substring(0, i);
-            texto.text = currentText;
-            yield return new WaitForSeconds(delay);
-        }
-    }
-}
+//     // Start is called before the first frame update
+//     void Start()
+//     {
+//         fullText = "hola que tal dios que guapo esto.";
+//         fullTextMin1 = "hola que tal dios que guapo esto";
+
+//         StartCoroutine(ShowText());
+//     }
+//     void Update()
+//     {
+//         if (texto.text.Equals(fullTextMin1))
+//         {
+//             done = true;
+//         }
+//         else
+//         {
+//             done = false;
+//         }
+//     }
+
+//     IEnumerator ShowText()
+//     {
+//         Debug.Log(" currentText " + currentText);
+//         if (done)
+//         {
+//             Debug.Log(" currentText1 " + currentText);
+
+//             currentText = "";
+//         }
+//         Debug.Log(" currentText1 " + currentText);
+//         for (int i = 0; i < fullText.Length; i++)
+//         {
+//             currentText = fullText.Substring(0, i);
+//             texto.text = currentText;
+//             yield return new WaitForSeconds(delay);
+//         }
+//     }
+// }
