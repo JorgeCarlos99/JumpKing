34037c3a9217f758b5900f91a1eb06a1ef40b4de
JorgeCarlos99 <o.sanjorge1@gmail.com>
2022-12-14 20:01:56 +0100

    texto que desaparezca ultimo nivel acuerdatelanzas

--- Assets/Scripts/LevelManagement/Level9.cs
+++ Assets/Scripts/LevelManagement/Level9.cs
@@ -4,12 +4,26 @@ using UnityEngine;
 
 public class Level9 : MonoBehaviour
 {
+    public GameObject canvasText;
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Player")
         {
             Vector3 changeToLevel3 = new Vector3(-3f, 2530, 0);
             Camera.main.gameObject.transform.position = changeToLevel3;
+            if (SpearCounter.instance.spears < 2)
+            {
+                Debug.Log("You have 2 Spears so the barrier is destroied");
+                canvasText.SetActive(true);
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            canvasText.SetActive(false);
         }
     }
 }
